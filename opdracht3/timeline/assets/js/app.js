72fb0b23dbcea45a7666d11b35cfefaab0be8564
Jelle Overbeek <j.overbeek@recognize.nl>
2018-03-26 15:16:22 +0200

    absolute positions are now being calculated

--- opdracht3/timeline/assets/js/app.js
+++ opdracht3/timeline/assets/js/app.js
@@ -1,23 +1,40 @@
-// Feature detect queryselector, classlist and eventlistener
-if(document.querySelector && document.addEventListener && ('classList' in document.body)) {
+if( document.querySelector
+    && document.addEventListener
+    && ('classList' in document.body)
+    && document.body.getBoundingClientRect()) {
+
+    document.body.classList.add("js-supported");
 
     var timeline = {
         wrapper: document.querySelector(".timeline"),
         items: document.querySelectorAll(".timeline-item"),
         years: [2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011],
+        calcPositions: function() {
+            var yearElements = document.querySelectorAll(".timeline-path__year");
+
+            for(var i = 0; i < yearElements.length; i++) {
+                var top = yearElements[i].offsetTop;
+
+                for(var x = 0; x < this.items.length; x++ ) {
+                    if(this.items[x].dataset.yearEnd === yearElements[i].dataset.year) {
+                        var rect = this.items[x].getBoundingClientRect();
+                        this.items[x].style.top = top - (rect.height / 2) + "px";
+                    }
+                }
+            }
+        },
         addPath: function () {
             var list = '<div class="timeline-path__start">Now</div>';
 
             for (var i = 0; i < this.years.length; i++) {
                 list += '<li class="timeline-path__year" data-year="'+ this.years[i] +'">'+ this.years[i] + '</li>';
-                console.log();
             }
 
             this.wrapper.insertAdjacentHTML("beforeend", '<ul class="timeline-path">' + list + '</ul>');
         },
         init: function () {
-            document.body.classList.add("js-supported");
             this.addPath();
+            this.calcPositions();
         }
     };
 
