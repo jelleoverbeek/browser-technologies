4da623c9c2990eadb5156da89dad0e2917fd0761
Jelle Overbeek <j.overbeek@recognize.nl>
2018-03-28 10:59:45 +0200

    added scroll position check

--- opdracht3/timeline/assets/js/app.js
+++ opdracht3/timeline/assets/js/app.js
@@ -9,16 +9,60 @@ if( document.querySelector
         wrapper: document.querySelector(".timeline"),
         items: document.querySelectorAll(".timeline-item"),
         years: [2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011],
-        calcPositions: function() {
-            var yearElements = document.querySelectorAll(".timeline-path__year");
+        yearElements: [],
+        setFixed: function () {
 
-            for(var i = 0; i < yearElements.length; i++) {
-                var top = yearElements[i].offsetTop;
+        },
+        getBeforeHeight: function (element) {
+            var height = window.getComputedStyle(element, ':before').getPropertyValue('height');
+            height = height.replace("px", "");
+            return height - 0;
+        },
+        getTop: function (year) {
+            var top;
+
+            for(var i = 0; i < this.yearElements.length; i++) {
+                var yearElement = this.yearElements[i];
+
+                if(year === yearElement.dataset.year) {
+                    top = yearElement.offsetTop + this.getBeforeHeight(yearElement);
+                }
+            }
+
+            return top
+        },
+        checkPosition: function (ev) {
+            for(var i = 0; i < this.items.length; i++ ) {
+                var item = this.items[i];
+                var rect = item.getBoundingClientRect();
+
+                var startTop = this.getTop(item.dataset.yearStart);
+                var endTop = this.getTop(item.dataset.yearEnd);
+                var xCenter = window.innerHeight / 2;
+                var scrollPos = window.scrollY - this.wrapper.offsetTop + xCenter;
+
+                console.log(startTop, endTop, window.scrollY);
+
+                if(scrollPos > endTop && scrollPos < startTop) {
+                    item.style.position = "fixed";
+                    item.style.top = xCenter + "px";
+                } else {
+                    item.style.position = "absolute";
+                    /*item.style.top = startTop + "px";*/
+                }
+
+            }
+        },
+        calcPositions: function() {
+            this.yearElements = document.querySelectorAll(".timeline-path__year");
+
+            for(var i = 0; i < this.yearElements.length; i++) {
+                var top = this.getTop(this.yearElements[i].dataset.year);
 
                 for(var x = 0; x < this.items.length; x++ ) {
-                    if(this.items[x].dataset.yearEnd === yearElements[i].dataset.year) {
+                    if(this.items[x].dataset.yearEnd === this.yearElements[i].dataset.year) {
                         var rect = this.items[x].getBoundingClientRect();
-                        this.items[x].style.top = top - (rect.height / 2) + "px";
+                        this.items[x].style.top = top + "px";
                     }
                 }
             }
@@ -33,6 +77,12 @@ if( document.querySelector
             this.wrapper.insertAdjacentHTML("beforeend", '<ul class="timeline-path">' + list + '</ul>');
         },
         init: function () {
+            _this = this;
+
+            window.addEventListener("scroll", function(ev) {
+                _this.checkPosition(ev);
+            });
+
             this.addPath();
             this.calcPositions();
         }
